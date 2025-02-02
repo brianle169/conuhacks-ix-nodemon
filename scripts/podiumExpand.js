cd988e738457626de5de6ffc7c154b3da29abcee
Brian Le <83758821+brianle169@users.noreply.github.com>
2025-02-02 05:22:51 -0500

    Fixed minor styling issue

--- scripts/podiumExpand.js
+++ scripts/podiumExpand.js
@@ -1,24 +1,21 @@
-// try {
-//     let scores = JSON.parse(localStorage.getItem('scores')) || [];
-// } catch (error) {
-//     console.log(error);
-    
-// }
-
+try {
+  let scores = JSON.parse(localStorage.getItem("scores")) || [];
+} catch (error) {
+  console.log(error);
+}
 
 function updateScores() {
-    // Get the ordered list by ID
-    let rankingList = document.getElementById("player-ranking-list");
+  // Get the ordered list by ID
+  let rankingList = document.getElementById("player-ranking-list");
 
-    // empty the ranking list to update it
-    rankingList.innerHTML = "";
+  // empty the ranking list to update it
+  rankingList.innerHTML = "";
 
-    // Loop through the scores and add them to the list
-    scores.forEach(item => {
-        let listItem = document.createElement("li");
-        // listItem.textContent = `${item.name} - ${item.score} points - ${item.attemptDate} - attempt #${item.attemptNumber}`;
-        listItem.innerHTML = 
-        `<table class="score-data">
+  // Loop through the scores and add them to the list
+  scores.forEach((item) => {
+    let listItem = document.createElement("li");
+    // listItem.textContent = `${item.name} - ${item.score} points - ${item.attemptDate} - attempt #${item.attemptNumber}`;
+    listItem.innerHTML = `<table class="score-data">
             <tr>
                 <td>${item.name}</td>
                 <td>${item.score} points</td>
@@ -27,27 +24,27 @@ function updateScores() {
                 <td>attempt #${item.attemptNumber}</td>
             </tr>
         </table>`;
-        rankingList.appendChild(listItem);
-    });
+    rankingList.appendChild(listItem);
+  });
 }
 
-
-
-
-
-
-
 function expandPodium() {
-    let questionPanelElement = parent.window.document.querySelector(".js-question-panel");
-    if (questionPanelElement) {
-        questionPanelElement.style.visibility = 'hidden';
-        questionPanelElement.style.display = 'none';
-    }
-    console.log('podium expanded!');
-    updateScores();
-    
+  let iframe = document.getElementById("podium");
+  console.log(iframe);
+  let questionPanelElement =
+    iframe.contentDocument.querySelector(".js-question-panel");
+  //   let questionPanelElement = document.querySelector(".js-question-panel");
+  // parent.window.document.querySelector(".question-panel");
+  console.log(questionPanelElement);
+  // parent.window.document.querySelector(".js-question-panel");
+  if (questionPanelElement) {
+    questionPanelElement.style.visibility = "hidden";
+    questionPanelElement.style.display = "none";
+  }
+  console.log("podium expanded!");
+  updateScores();
 }
 let expandButtonElement = document.querySelector(".js-expand-button");
 if (expandButtonElement) {
-    expandButtonElement.addEventListener("click", expandPodium);
+  expandButtonElement.addEventListener("click", expandPodium);
 }
