b9a48876871d164cf65d714956a6c6452c15a94e
tonybarrak <197407297+tonybarrak@users.noreply.github.com>
2025-02-02 03:33:35 -0500

    podium expansion change display

--- scripts/podium.js
+++ scripts/podium.js
@@ -2,35 +2,36 @@
 let scores = JSON.parse(localStorage.getItem('scores')) || [];
 let numberOfAttempts = 1;
 // let score1 = {name:"guest", score:"400", attemptNumber: 1, attemptDate: new Date(2025, 2, 1, 9, 56, 30, 0)};
-scores.push({name:"guest", score:"400", attemptNumber: 5, attemptDate: new Date(2025, 2, 1, 9, 56, 30, 0)});
-scores.push({name:"guest", score:"370", attemptNumber: 3, attemptDate: new Date(2025, 2, 1, 10, 31, 30, 0)});
-scores.push({name:"guest", score:"350", attemptNumber: 4, attemptDate: new Date(2025, 2, 1, 10, 31, 30, 0)});
-scores.push({name:"guest", score:"330", attemptNumber: 1, attemptDate: new Date(2025, 2, 1, 10, 31, 30, 0)});
-scores.push({name:"guest", score:"300", attemptNumber: 2, attemptDate: new Date(2025, 2, 1, 10, 31, 30, 0)});
+scores.push({name:"guest", score:"400", language: "c++",attemptNumber: 5, attemptDate: new Date(2025, 2, 1, 9, 56, 30, 0)});
+scores.push({name:"guest", score:"370", language: "java",attemptNumber: 3, attemptDate: new Date(2025, 2, 1, 10, 31, 30, 0)});
+scores.push({name:"guest", score:"350", language: "python",attemptNumber: 4, attemptDate: new Date(2025, 2, 1, 10, 31, 30, 0)});
+scores.push({name:"guest", score:"330", language: "c#",attemptNumber: 1, attemptDate: new Date(2025, 2, 1, 10, 31, 30, 0)});
+scores.push({name:"guest", score:"300", language: "javascript",attemptNumber: 2, attemptDate: new Date(2025, 2, 1, 10, 31, 30, 0)});
 
+updateScoresSmallDisplay();
 
-// console.log(scores);
+/**
+ * Function to update the content of the podium when it's not expanded.
+ */
+function updateScoresSmallDisplay() {
+    // Get the ordered list by ID
+    let rankingList = document.getElementById("player-ranking-list");
 
-// Get the ordered list by ID
-let rankingList = document.getElementById("player-ranking-list");
+    // Loop through the scores and add them to the list
+    scores.forEach(item => {
+        let listItem = document.createElement("li");
+        // listItem.textContent = `${item.name} - ${item.score} points - ${item.attemptDate} - attempt #${item.attemptNumber}`;
+        listItem.innerHTML = 
+        `<table class="score-data">
+            <tr>
+                <td>${item.name}</td>
+                <td>${item.score} points</td>
+            </tr>
+        </table>`;
+        rankingList.appendChild(listItem);
+    });
+}
 
-// Loop through the scores and add them to the list
-scores.forEach(item => {
-    let listItem = document.createElement("li");
-    // listItem.textContent = `${item.name} - ${item.score} points - ${item.attemptDate} - attempt #${item.attemptNumber}`;
-    listItem.innerHTML = 
-    `<table class="score-data">
-        <tr>
-            <td>${item.name}</td>
-            <td>${item.score} points</td>
-            <!--
-            <td>${item.attemptDate}</td>
-            <td>attempt #${item.attemptNumber}</td>
-            -->
-        </tr>
-    </table>`;
-    rankingList.appendChild(listItem);
-});
 
 /**
  * Function to sort players by score in descending order.
--- scripts/podiumExpand.js
+++ scripts/podiumExpand.js
@@ -1,3 +1,36 @@
+if (!scores) {
+    let scores = JSON.parse(localStorage.getItem('scores')) || [];
+}
+
+function updateScores() {
+    // Get the ordered list by ID
+    let rankingList = document.getElementById("player-ranking-list");
+
+    // empty the ranking list to update it
+    rankingList.innerHTML = "";
+
+    // Loop through the scores and add them to the list
+    scores.forEach(item => {
+        let listItem = document.createElement("li");
+        // listItem.textContent = `${item.name} - ${item.score} points - ${item.attemptDate} - attempt #${item.attemptNumber}`;
+        listItem.innerHTML = 
+        `<table class="score-data">
+            <tr>
+                <td>${item.name}</td>
+                <td>${item.score} points</td>
+                <td style="width: 130px">${item.language}</td>
+                <td>${item.attemptDate}</td>
+                <td>attempt #${item.attemptNumber}</td>
+            </tr>
+        </table>`;
+        rankingList.appendChild(listItem);
+    });
+}
+
+
+
+
+
 
 
 function expandPodium() {
@@ -7,7 +40,7 @@ function expandPodium() {
         questionPanelElement.style.display = 'none';
     }
     console.log('podium expanded!');
-    
+    updateScores();
     
 }
 let expandButtonElement = document.querySelector(".js-expand-button");
